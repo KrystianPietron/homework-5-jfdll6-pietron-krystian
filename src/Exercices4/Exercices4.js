2cc44c1a27f55ef4456b10c55f27a0448423715a
Krystian Pietron <krystianpietr@gmail.com>
2018-11-25 09:58:51 +0100

    add function for buttons

--- src/Exercices4/Exercices4.js
+++ src/Exercices4/Exercices4.js
@@ -2,43 +2,47 @@ import React from 'react'
 import Button from './Button'
 
 class Exercices4 extends React.Component {
-    state={
+    state = {
         number: 0
     }
-    incHandler(){
-
-    }
-    inc5Handler(){
-
-    }
-    decHandler(){
-
-    }
-    dec5Handler(){
-        
-    }
-    render(){
-        return(
+    incHandler = () => (
+        this.setState({ number: this.state.number + 1 })
+    )
+    incHandler5 = () => (
+        this.setState({ number: this.state.number + 5 })
+    )
+    decHandler = () => (
+        this.setState({ number: this.state.number - 1 })
+    )
+    decHandler5 = () => (
+        this.setState({ number: this.state.number - 5 })
+    )
+    resetHandler = () => (
+        this.setState({ number: this.state.number = 0 })
+    )
+    render() {
+        return (
             <div>
-                <Button 
-                label='+'
-                onClick={()=>{}}
-                />
-                <Button 
-                label='-'
-                onClick={()=>{}}
+                <h2>{this.state.number}</h2>
+                <Button
+                    label='+'
+                    onClick={this.incHandler}
                 />
                 <Button
-                label='+ 5' 
-                onClick={()=>{}}
+                    label='-'
+                    onClick={this.decHandler}
                 />
-                <Button 
-                label='- 5'
-                onClick={()=>{}}
+                <Button
+                    label='+ 5'
+                    onClick={this.incHandler5}
                 />
-                <Button 
-                label='Reset'
-                onClick={()=>{}}
+                <Button
+                    label='- 5'
+                    onClick={this.decHandler5}
+                />
+                <Button
+                    label='Reset'
+                    onClick={this.resetHandler}
                 />
             </div>
         )
