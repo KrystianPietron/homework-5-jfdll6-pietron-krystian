96cae28425b7b867ce66048945e6863bee7c32fe
Krystian Pietron <krystianpietr@gmail.com>
2018-11-25 15:18:39 +0100

    add function jumpTo

--- src/Exercices1/Game.js
+++ src/Exercices1/Game.js
@@ -8,6 +8,7 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            stepNumber: 0,
             xIsNext: true
         }
     }
@@ -29,6 +30,14 @@ class Game extends React.Component {
                     ]
                 ),
                 xIsNext: !this.state.xIsNext
+            }
+        )
+    }
+    jumpTo(step) {
+        this.setState(
+            {
+                stepNumber: step,
+                xIsNext: (step % 2) === 0
             }
         )
     }
