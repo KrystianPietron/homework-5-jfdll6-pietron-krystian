7bfce0cd716570a73653c73ac2f9a9c09268546c
Krystian Pietron <krystianpietr@gmail.com>
2018-11-25 13:41:52 +0100

    add constructor for Game class

--- src/Exercices1/Game.js
+++ src/Exercices1/Game.js
@@ -2,6 +2,15 @@ import React from 'react'
 import Board from './Board'
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            history: [{
+                squares: Array(9).fill(nill)
+            }],
+            xIsNext: true
+        }
+    }
     render() {
         return (
             <div className="game">
