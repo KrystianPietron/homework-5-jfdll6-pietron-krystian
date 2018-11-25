c9fee77118f88d126b02c0ff6a44c5242f2b8e19
Krystian Pietron <krystianpietr@gmail.com>
2018-11-25 13:58:47 +0100

    copy funtion handleClick and calculateWinner to Game.js and add history

--- src/Exercices1/Board.js
+++ src/Exercices1/Board.js
@@ -2,32 +2,10 @@ import React from 'react'
 import Square from './Square'
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true
-        }
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice()
-        if (this.calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : "O"
-        this.setState(
-            {
-                squares: squares,
-                xIsNext: !this.state.xIsNext
-            }
-        )
-    }
-
     renderSquare(i) {
         return <Square
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
         />;
     }
     calculateWinner(squares) {
@@ -49,7 +27,7 @@ class Board extends React.Component {
         }
         return null;
     }
-    
+
     render() {
         const winner = this.calculateWinner(this.state.squares)
         let status;
@@ -60,7 +38,6 @@ class Board extends React.Component {
         }
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
